6ddd4d3c5b1acc71c0e56500738beabf60805403
yanniks <kontakt@yanniks.de>
2013-11-04 20:18:08 +0100

    fix compiling
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := ControlCenter
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
 
-LOCAL_STATIC_JAVA_LIBRARIES := androidsupport
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androidsupport:libs/android-support-v4.jar
-
-include $(BUILD_MULTI_PREBUILT)
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
